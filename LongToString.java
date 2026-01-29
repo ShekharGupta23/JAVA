880356ba93c3302363e049c63d1333a2d3fbcff9
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-29 15:04:58 +0530

    Add files via upload

---
+++ LongToString.java
@@ -0,0 +1,17 @@
+// Java program to convert Long to String using + operator
+public class LongToString {
+
+    public static void main(String args[])
+    {
+
+        Long varLong = 999999999999L;
+
+        String str = varLong + " ";
+
+
+        System.out.println("Converted type : "
+                           + str.getClass().getName());
+
+        System.out.println(str);
+    }
+}
