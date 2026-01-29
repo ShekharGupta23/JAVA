cfeaf571b0c90b1108bb15818047c3b9a03871ce
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-29 15:08:54 +0530

    Add files via upload

---
+++ CharToInt.java
@@ -0,0 +1,15 @@
+// Java Program to Convert Char to Int Using ASCII value 
+class CharToInt
+{
+    public static void main(String[] args)
+    {
+
+        char num = '3';
+
+        System.out.println("char value: " + num);
+
+        int n = num - '0';
+
+        System.out.println("int value: " + n);
+    }
+}
