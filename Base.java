632ba7fd7d07d0e80d9901391d6dc6807e64e456
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-31 15:09:24 +0530

    Java Abstract Class

---
+++ Base.java
@@ -0,0 +1,24 @@
+abstract class Sunstar {
+    abstract void printInfo();
+}
+
+class Employee extends Sunstar {
+    void printInfo()
+    {
+        String name = "Shekhar";
+        int age = 20;
+        float salary = 222.2F;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
+
+class Base {
+    public static void main(String args[])
+    {
+        Sunstar s = new Employee();
+        s.printInfo();
+    }
+}
